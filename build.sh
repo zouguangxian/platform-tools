f1c6bd7e80b3895611d34d3625f2f309da878bd5
Zou Guangxian <zouguangxian@gmail.com>
2023-05-11 23:07:52 +0800

    feat: clone repository once only
    - set HOST_TRIPLE based on the type of the current hardware platform
    - clone repository only once
    - reset work directory and switch tag

--- build.sh
+++ build.sh
@@ -13,36 +13,46 @@ case "${unameOut}" in
         ARTIFACT=solana-bpf-tools-windows.tar.bz2;;
     Linux* | *)
         EXE_SUFFIX=
-        HOST_TRIPLE=x86_64-unknown-linux-gnu
-        ARTIFACT=solana-bpf-tools-linux.tar.bz2
+        HOST_TRIPLE=$(uname -m)-unknown-linux-gnu
+        ARTIFACT=solana-bpf-tools-$(uname -s)-$(uname -m).tar.bz2
 esac
 
 cd "$(dirname "$0")"
 OUT_DIR=$(realpath "${1:-out}")
 
-rm -rf "${OUT_DIR}"
 mkdir -p "${OUT_DIR}"
 pushd "${OUT_DIR}"
 
-git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/rust.git
+if [ ! -d "rust" ]; then
+    git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/rust.git
+fi
 echo "$( cd rust && git rev-parse HEAD )  https://github.com/solana-labs/rust.git" >> version.md
 
-git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/cargo.git
+if [ ! -d "cargo" ]; then
+    git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/cargo.git
+fi
 echo "$( cd cargo && git rev-parse HEAD )  https://github.com/solana-labs/cargo.git" >> version.md
 
 pushd rust
+git reset --hard HEAD && git clean -d -f -x && git fetch --tags && git checkout bpf-tools-v1.29
 ./build.sh
 popd
 
 pushd cargo
+git reset --hard HEAD && git clean -d -f -x && git fetch --tags && git checkout bpf-tools-v1.29
 OPENSSL_STATIC=1 cargo build --release
 popd
 
 if [[ "${HOST_TRIPLE}" != "x86_64-pc-windows-msvc" ]] ; then
-    git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/newlib.git
+    if [ ! -d "newlib" ]; then
+        git clone --single-branch --branch bpf-tools-v1.29 https://github.com/solana-labs/newlib.git
+    fi
     echo "$( cd newlib && git rev-parse HEAD )  https://github.com/solana-labs/newlib.git" >> version.md
     mkdir -p newlib_build
     mkdir -p newlib_install
+    pushd newlib
+    git reset --hard HEAD && git clean -d -f -x && git fetch --tags && git checkout bpf-tools-v1.29
+    popd
     pushd newlib_build
     CC="${OUT_DIR}/rust/build/${HOST_TRIPLE}/llvm/bin/clang" \
       AR="${OUT_DIR}/rust/build/${HOST_TRIPLE}/llvm/bin/llvm-ar" \
